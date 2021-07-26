5a43f744246bd27b1fc4b467835147d26d962651
Adriano Heller Mylla <adriano@technoizz.com>
2021-07-25 22:27:59 -0300

    fix: Refactor twoNums algo logic

--- twoNums.mjs
+++ twoNums.mjs
@@ -1,23 +1,29 @@
-const twoNumSum = (array) => {
-    let sum = 0;
+/**
+ * 
+ * @param array 
+ * @param sum 
+ * @returns matched or not by pair
+ */
+const twoNumSum = (array,sum) => {
     let composedArray = [];
-    if(typeof array === 'object' && array instanceof Array && array.length > 2){
-        for(let x = 0; x < array.length; x++){
+    if (typeof array === 'object' && array instanceof Array && array.length > 2) {
+        for (let x = 0; x < array.length - 1; x++) {
             let reference = x;
-            let compared = reference + 2;
-            composedArray.push([array[reference],array[compared]]);
-            
+            let compared = reference + 1;
+            composedArray.push([array[reference], array[compared]]);
         };
-    return composedArray;    
-        
-    }else{
+        const filteredArrayItems = composedArray.filter(item => typeof item !== undefined);
+        const summedPairs = filteredArrayItems.filter(array => (array[0] + array[1]) === sum);
+        if(summedPairs.length > 0) return summedPairs;
         return false;
-    };
+    } else {
+        return false;
+    };    
 };
 
 
-const list = [12,3,-15,7,113];
-const sum = 19;
+const list = [12, 3, -15, 7, 113];
+const sum = 15;
 
-console.log(twoNumSum(list));
+console.log(twoNumSum(list,sum));
 
